3071d5d3f4389cea341e5e9265d2f1f63e31f798
theopenem <46750331+theopenem@users.noreply.github.com>
2022-12-09 16:25:32 -0500

    change mfa to sha1

--- Toems-Service/Entity/ServiceUser.cs
+++ Toems-Service/Entity/ServiceUser.cs
@@ -263,7 +263,7 @@ namespace Toems_Service.Entity
         public string GenerateTempMfaSecret(int userId)
         {
             var u = GetUserWithPass(userId);
-            var mfa = new TwoFactorAuth("Theopenem", 6, 60, Algorithm.SHA512, new ToemsQrProvider());
+            var mfa = new TwoFactorAuth("Theopenem", 6, 30, Algorithm.SHA1, new ToemsQrProvider());
             
             var secret = mfa.CreateSecret(160);
             u.MfaTempSecret = new EncryptionServices().EncryptText(secret);
