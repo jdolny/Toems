5dd9d30f57b5061b847e40ecda11295a4b5d8225
jdolny <46750331+jdolny@users.noreply.github.com>
2023-06-12 11:21:50 -0400

    Revert "Revert "start winget integration""
    This reverts commit 67eb01e08a6fb64269d66ca82700eae238244e96.

---
+++ Toems-FrontEnd/views/admin/winget.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Toems_FrontEnd.views.admin
+{
+    public partial class winget : BasePages.Admin
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRunWinGet_Click(object sender, EventArgs e)
+        {
+            Call.SettingApi.RunWinGetImporter();
+        }
+    }
+}
